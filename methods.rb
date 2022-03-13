cf3af747200edcaeda2310fbf15b7f956c1218ae
Stephen Talley <stvlley.amzn@gmail.com>
2022-03-13 16:53:15 -0400

    commit tree 3e0a3294a3113fa29c2633a27d97fbbd24447c22

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen")
+
+def add(x, y)
+    return x + y
+end
+
+add(2, 5)
+
+
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+
+    return num / 2
+end
+
+halve(30)
